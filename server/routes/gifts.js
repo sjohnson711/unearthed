3b1dc9151e18f2ba04238f9baa6690896d1f4982
Seth <samaritanbrotherseth@gmail.com>
2026-06-07 14:19:35 -0400

    completed Unit 2 assignment with setting up Render, added a seeding file, updated scripts in package.json to run the config file before running the script and nodemon being able to update.

--- server/routes/gifts.js
+++ server/routes/gifts.js
@@ -1,18 +1,13 @@
 import express from "express";
-
 import path from "path";
-
 import { fileURLToPath } from "url";
-
-import giftData from "../data/gifts.js";
+import GiftsController from "../controllers/gifts.js"
 
 const _filename = fileURLToPath(import.meta.url);
 const _dirname = path.dirname(_filename);
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json(giftData);
-});
+router.get("/", GiftsController.getGifts);
 
 router.get("/:giftId", (req, res) => {
   res.status(200).sendFile(path.resolve(_dirname, "../public/gift.html"));
